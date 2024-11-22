4efbe96f222dee661bc833c5e61574af2bd0fcd3
kistenklaus <karlsasssie@gmail.com>
2024-11-22 14:12:33 +0100

    create hypo1-hypo4

---
+++ hypo1.R
@@ -0,0 +1,26 @@
+
+#### Hypothesis
+# 1. Games with higher rating more likely end with resign than games with lower rating
+
+
+library(remotes)
+options(repos = "https://cloud.r-project.org/")
+if (!require("easypackages")) {
+  install.packages("easypackages")
+  library(easypackages)
+} else {
+  library(easypackages)
+}
+packages(
+  "tidyverse",
+  "tidytuesdayR",
+  "dplyr",
+  "readr",
+  prompt = FALSE
+)
+
+tuesday_chess <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-10-01/chess.csv')
+
+chess <- tuesday_chess %>%
+  dplyr::filter(rated=TRUE) %>%
+  dplyr::select(winner, white_rating, black_rating, white_id, black_id, end_time)
---
+++ hypo2.R
@@ -0,0 +1,28 @@
+
+
+#### Hypothesis
+# 2. At lower ratings the difference between black and white is neglegible
+
+
+
+library(remotes)
+options(repos = "https://cloud.r-project.org/")
+if (!require("easypackages")) {
+  install.packages("easypackages")
+  library(easypackages)
+} else {
+  library(easypackages)
+}
+packages(
+  "tidyverse",
+  "tidytuesdayR",
+  "dplyr",
+  "readr",
+  prompt = FALSE
+)
+
+tuesday_chess <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-10-01/chess.csv')
+
+chess <- tuesday_chess %>%
+  dplyr::filter(rated=TRUE) %>%
+  dplyr::select(winner, white_rating, black_rating, white_id, black_id, end_time)
